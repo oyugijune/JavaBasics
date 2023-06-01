fa78c3b847bc6967c5c66f0317237bfe300e005f
oyugijune <oyugijune863@gmail.com>
2023-06-01 09:34:31 +0300

    Used sentinel to control loop statement.

---
+++ src/SentinelControlledLoop.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class SentinelControlledLoop {
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a digit(if you enter 0 the programme will terminate)");
+        int number = input.nextInt();
+
+        int sum = 0;
+        while (number != 0) {
+            System.out.println("Enter a digit (if you enter 0 the programme will terminate ");
+            number = input.nextInt();
+            sum += number;
+        }
+        System.out.println("The sum is " + sum );
+
+    }
+}
